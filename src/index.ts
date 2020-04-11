134715987b8e82574080d97caa9b736620445f1b
Elias Platek <elias.platek@gmail.com>
2020-04-11 20:04:26 +0200

    Fixed configurable port number

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,18 @@ import "express-async-errors"
 import { setupApp } from "./middlewares"
 import { ytsPluginRouter } from "./routes/yts"
 import { oxTorrentPluginRouter } from "./routes/oxtorrent"
-
+const port = process.env.PORT || 5000
 const main = () => {
   const app = express()
 
   setupApp(app)
+    .get("", (req, res) => {
+      res.send({ status: "OK" })
+    })
     .use("/yts/v1", ytsPluginRouter)
     .use("/oxtorrent/v1", oxTorrentPluginRouter)
-    .listen(1337, () => {
-      console.log(`Listening on http://localhost:1337`)
+    .listen(port, () => {
+      console.log(`Listening on http://localhost:${port}`)
     })
 }
 
